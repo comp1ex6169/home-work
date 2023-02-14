83fe2bf6b5b8b9869c2a6a95a3be8351d367b7d6
comp1ex6169 <brightbest5270@gmail.com>
2023-02-14 08:17:47 +0700

    kuy

---
+++ hw3.py
@@ -0,0 +1,21 @@
+import turtle
+#turtle.forward(50)
+#turtle.right(45)
+#turtle.forward(50)
+#turtle.right(90)
+#turtle.forward(50)
+#turtle.right(45)
+#turtle.forward(50)
+#turtle.right(45)
+#turtle.forward(50)
+#turtle.right(90)
+#turtle.forward(50)
+for i in range(4):
+    turtle.forward(50)
+    turtle.right(45)
+    turtle.forward(50)
+    turtle.right(45)
+    
+
+turtle.done()
+
