6b81662e33151bcb05861f142fb71d0a56f5d682
comp1ex6169 <brightbest5270@gmail.com>
2023-03-18 23:50:09 +0700

    kuy

---
+++ hw7.py
@@ -0,0 +1,5 @@
+First = input("Enter first string: ")
+Second = input("Enter second string: ")
+Third = input("Enter third string: ")
+Final = First+Second+Third
+print("Whole string = "+Final.upper())
